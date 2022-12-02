2e4c68763f8884fe2a5abfa84d84d57404596ed9
aaronchenwei <aaronchenwei@hotmail.com>
2022-12-02 14:38:04 +0800

    Update build.gradle.kts

--- hello-jgrapht/build.gradle.kts
+++ hello-jgrapht/build.gradle.kts
@@ -20,8 +20,9 @@ dependencies {
 
   implementation(kotlin("stdlib"))
   testImplementation(kotlin("test"))
-  
+
   testImplementation(platform("org.junit:junit-bom:5.9.1"))
+  testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.withType<KotlinCompile> {
